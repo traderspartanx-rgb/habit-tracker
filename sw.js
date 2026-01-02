87bbb47ee710607033e5119c5682b28fb60d55f4
traderspartanx-rgb <traderspartanx@gmail.com>
2026-01-02 18:02:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,13 @@
-const cacheName = 'my-app-cache-v1';
-const assetsToCache = [
-  './index.html',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-// Install event
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assetsToCache);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('habit-cache').then(cache => {
+      return cache.addAll(['./']);
     })
   );
 });
 
-// Fetch event - offline support
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
